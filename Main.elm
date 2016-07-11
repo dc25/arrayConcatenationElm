c29d41f53196449071147ea9ed43a25d62db42cf
Dave Compton <davecompton7@gmail.com>
2016-07-11 04:50:26 +0000

    using onInput for more compact code.

--- Main.elm
+++ Main.elm
@@ -2,40 +2,28 @@ import Array as A exposing (append, fromList, toList)
 import String as S exposing (toList, fromList)
 import Html exposing (Html, Attribute, text, div, h1, h2, input)
 import Html.Attributes exposing (value)
-import Html.Events exposing (on, targetValue)
+import Html.Events exposing (onInput, targetValue)
 import Html.App exposing (beginnerProgram)
 import Json.Decode exposing (map)
 
-
 type alias Model = (String String)
 
-update newModel oldModel = newModel
+type Msg = Change0 String | Change1 String
 
-view : (String, String) -> Html (String, String)
+update msg (old0, old1) = 
+  case msg of
+    Change0 new0 -> (new0, old1)
+    Change1 new1 -> (old0, new1)
+
+view : (String, String) -> Html Msg
 view (string0, string1) =
   div []
     (  [ h1 [] [text "Array Concatenation"] ] 
-
     ++ [ h2 [] [text "Take two strings, convert them into arrays, concatenate the arrays, convert back to string, display result:" ] ] 
-
-    ++ [ input
-           [ value string0
-           , on "input" (map (\st -> (st, string1)) targetValue )
-           ]
-           []
-       ] 
-
+    ++ [ input [ value string0 , onInput Change0 ] [] ] 
     ++ [ text "+" ] 
-
-    ++ [ input
-           [ value string1
-           , on "input" (map (\st -> (string0, st)) targetValue)
-           ]
-           []
-       ] 
-
+    ++ [ input [ value string1 , onInput Change1 ] [] ] 
     ++ [ text "=" ] 
-
     ++ [ let array0 = A.fromList (S.toList string0)
              array1 = A.fromList (S.toList string1)
              joinedArray = A.append array0 array1
